b7bad64ab85703152ac8b60fae546ee8d0e2ba02
Trinhnp <trinhnp3@gmail.com>
2019-11-12 18:25:00 -0700

    Updates

---
+++ precache-manifest.083bcd1b93976d4e7dea62f347bc52d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac0581404b624fa458103d5e0f5c32b",
+    "url": "/calgary_recreation_location/index.html"
+  },
+  {
+    "revision": "5f9e887ba762842b5ab3",
+    "url": "/calgary_recreation_location/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "558a9d4c15dfd778abe7",
+    "url": "/calgary_recreation_location/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5f9e887ba762842b5ab3",
+    "url": "/calgary_recreation_location/static/js/2.e7f6ed73.chunk.js"
+  },
+  {
+    "revision": "558a9d4c15dfd778abe7",
+    "url": "/calgary_recreation_location/static/js/main.eca42a71.chunk.js"
+  },
+  {
+    "revision": "41662ff72438732ed084",
+    "url": "/calgary_recreation_location/static/js/runtime-main.cc13d3f6.js"
+  }
+]);
